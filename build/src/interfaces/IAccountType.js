76cce02234a1a46e239f9829b47a24653a0e6369
spikeblade <91207658+spikeblade@users.noreply.github.com>
2024-07-18 21:13:35 -0500

    clean the build

--- build/src/interfaces/IAccountType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccountScheme = void 0;
-const zod_1 = require("zod");
-exports.AccountScheme = zod_1.z.object({
-    id: zod_1.z.number().optional(),
-    accountOwner: zod_1.z.string().optional(),
-    Balance: zod_1.z.number().optional()
-});
